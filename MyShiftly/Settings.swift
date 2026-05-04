cf16ba494c74238b151fa3fe0d2a697074e7aa98
ballisingha <guriqbal@hotmail.de>
2026-05-04 23:25:25 +0200

    Refactored the models implemented singleton for settings the Shift model was refactored also for better calculation and  adding breaks

---
+++ MyShiftly/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  MyShiftly
+//
+//  Created by Guriqbal Singh Amroke on 04.05.26.
+//
+
+import Foundation
---
+++ MyShiftly/Shift.swift
@@ -0,0 +1,8 @@
+//
+//  Shift.swift
+//  MyShiftly
+//
+//  Created by Guriqbal Singh Amroke on 04.05.26.
+//
+
+import Foundation
