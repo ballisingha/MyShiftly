638e4820f0ace2d8e266ad796e564c57d4ebbec1
ballisingha <guriqbal@hotmail.de>
2026-05-12 22:30:40 +0200

    Fixed Swiftformat issues

--- MyShiftly/Views/Shifts/ShiftPreviewHelper.swift
+++ MyShiftly/Views/Shifts/ShiftPreviewHelper.swift
@@ -24,7 +24,7 @@ extension Shift {
                 startTime: .now.addingTimeInterval(-172_800),
                 endTime: .now.addingTimeInterval(-172_800 + 3600 * 2),
                 breaks: []
-            ),
+            )
         ]
     }
 }
